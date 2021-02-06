9aaf46d29635d289907355cb86cabe79779dcc23
hongker <xiaok2013@live.com>
2021-02-06 13:41:33 +0800

    Update number.go

--- number.go
+++ number.go
@@ -1,1 +1,23 @@
 package convert
+
+import "strconv"
+
+type number struct {
+	v int64
+}
+
+// 整形
+func Number(v int) *number {
+	return &number{v: int64(v)}
+}
+
+// 转字符串
+func (n *number) ToString() string {
+	return strconv.FormatInt(n.v, 10)
+}
+
+// 转float
+func (n *number) ToFloat() float64 {
+	return float64(n.v)
+}
+
