91aed9d081fd64e46c8b54255157f28b9a606cd6
min.chen <min.chen@yingxiong.com>
2021-02-06 16:19:06 +0800

    json to map、struct

--- json.go
+++ json.go
@@ -2,16 +2,19 @@ package convert
 
 import "encoding/json"
 
+// json类型
 type Json string
 
 func (s Json) ToByte() []byte {
 	return stringToByte(string(s))
 }
 
+// 转struct, v需要为指针
 func (s Json) ToStruct(v interface{}) error {
 	return json.Unmarshal(s.ToByte(), v)
 }
 
+// 转map
 func (s Json) ToMap() (map[string]interface{}, error) {
 	item := make(map[string]interface{})
 	err := s.ToStruct(&item)
