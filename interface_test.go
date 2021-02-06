65b9460ef6fa0efe6abc5f8a257a6aecfd939a09
hongker <xiaok2013@live.com>
2021-02-06 10:41:22 +0800

    Update interface_test.go

--- interface_test.go
+++ interface_test.go
@@ -7,47 +7,47 @@ import (
 
 func TestInterface_Int(t *testing.T) {
 	var num int = 1
-	assert.Equal(t, NewInterface("1").Int(), num)
-	assert.Equal(t, NewInterface(1).Int(), num)
-	assert.Equal(t, NewInterface(1.0).Int(), num)
-	assert.Equal(t, NewInterface(uint(1)).Int(), num)
-	assert.Equal(t, NewInterface(true).Int(), num)
+	assert.Equal(t, Interface("1").ToInt(), num)
+	assert.Equal(t, Interface(1).ToInt(), num)
+	assert.Equal(t, Interface(1.0).ToInt(), num)
+	assert.Equal(t, Interface(uint(1)).ToInt(), num)
+	assert.Equal(t, Interface(true).ToInt(), num)
 }
 
 func BenchmarkInterface_Int(b *testing.B) {
 	var a int64 = 1
-	inter := NewInterface(a)
+	inter := Interface(a)
 	for i := 0; i < b.N; i++ {
-		inter.Int()
+		inter.ToInt()
 	}
 }
 
 func TestInterface_String(t *testing.T) {
 	var str = "1"
-	assert.Equal(t, str, NewInterface(1).String())
-	assert.Equal(t, "1.12", NewInterface(1.12).String())
-	assert.Equal(t, str, NewInterface(uint64(1)).String())
-	assert.Equal(t, "true", NewInterface(true).String())
-	assert.Equal(t, "false", NewInterface(false).String())
+	assert.Equal(t, str, Interface(1).ToString())
+	assert.Equal(t, "1.12", Interface(1.12).ToString())
+	assert.Equal(t, str, Interface(uint64(1)).ToString())
+	assert.Equal(t, "true", Interface(true).ToString())
+	assert.Equal(t, "false", Interface(false).ToString())
 }
 
 func BenchmarkInterface_String(b *testing.B) {
-	inter := NewInterface(1)
+	inter := Interface(1)
 	for i := 0; i < b.N; i++ {
-		_ = inter.String()
+		_ = inter.ToString()
 	}
 }
 
 func TestInterface_Float64(t *testing.T) {
-	assert.Equal(t, 1.1, NewInterface(1.1).Float64())
-	assert.Equal(t, 1.2, NewInterface("1.2").Float64())
-	assert.Equal(t, float64(1), NewInterface(int32(1)).Float64())
-	assert.Equal(t, float64(1), NewInterface(uint(1)).Float64())
+	assert.Equal(t, 1.1, Interface(1.1).ToFloat())
+	assert.Equal(t, 1.2, Interface("1.2").ToFloat())
+	assert.Equal(t, float64(1), Interface(int32(1)).ToFloat())
+	assert.Equal(t, float64(1), Interface(uint(1)).ToFloat())
 }
 
 func BenchmarkInterface_Float64(b *testing.B) {
-	inter := NewInterface(1)
+	inter := Interface(1)
 	for i := 0; i < b.N; i++ {
-		inter.Float64()
+		inter.ToFloat()
 	}
 }
